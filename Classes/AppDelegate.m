c280b7be88e9e99d44da7b2994ac0a5d9b7a65df
Marco Abundo <mabundo@gmail.com>
2011-03-30 15:01:42 -0700

    Updated license boilderplate

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Marco Abundo
+ * Copyright 2011 Marco Abundo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
